b5fb75bb17a16843eca10297a112d84e1a94abd6
Matd127 <79983536+Matd127@users.noreply.github.com>
2021-06-17 23:02:40 +0200

    17.06.2021

--- scripts.js
+++ scripts.js
@@ -11,3 +11,40 @@ window.onscroll = function(){
       header.classList.remove('active');
     }
 }
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plus(n) {
+  showSlides(slideIndex += n);
+}
+
+function current(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) 
+{
+  var i;
+  var slides = document.getElementsByClassName("slides");
+  if (n > slides.length) 
+      slideIndex = 1  
+      
+  if (n < 1) 
+    slideIndex = slides.length
+
+  for (i = 0; i < slides.length; i++)
+      slides[i].style.display = "none";  
+
+  slides[slideIndex-1].style.display = "block";  
+
+}
+
+
+function validate(){
+  var x = document.getElementById('imie').value;
+
+  if(imie=="")
+    alert("Wpisz coś!");
+}
+
